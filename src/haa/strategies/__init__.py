0bd9f3460a5ca78c1ab85f501b27c6c5223593b7
danwhite-lab <danielcwhite@gmail.com>
2026-09-21 14:23:34 +0300

    Add Classic HAA strategy without QQQ

--- src/haa/strategies/__init__.py
+++ src/haa/strategies/__init__.py
@@ -1,4 +1,5 @@
+from .haa_classic_no_qqq import HAAClassicNoQQQ
 from .haa_simple import HAASimple
 from .haa_simple_leveraged_2x import HAASimpleLeveraged2x
 
-__all__ = ["HAASimple", "HAASimpleLeveraged2x"]
+__all__ = ["HAAClassicNoQQQ", "HAASimple", "HAASimpleLeveraged2x"]
