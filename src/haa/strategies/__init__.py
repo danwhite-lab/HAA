9c680d3b17d3d28e38fb8dd217a8a2e615cc6260
danwhite-lab <danielcwhite@gmail.com>
2026-09-21 20:09:41 +0300

    Export leveraged Classic HAA strategy

--- src/haa/strategies/__init__.py
+++ src/haa/strategies/__init__.py
@@ -1,5 +1,6 @@
 from .haa_classic_no_qqq import HAAClassicNoQQQ
+from .haa_classic_leveraged_no_qqq import HAAClassicLeveragedNoQQQ
 from .haa_simple import HAASimple
 from .haa_simple_leveraged_2x import HAASimpleLeveraged2x
 
-__all__ = ["HAAClassicNoQQQ", "HAASimple", "HAASimpleLeveraged2x"]
+__all__ = ["HAAClassicNoQQQ", "HAAClassicLeveragedNoQQQ", "HAASimple", "HAASimpleLeveraged2x"]
