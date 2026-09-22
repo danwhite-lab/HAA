5f2e58ac121dd7392c9ac4f6ca40acae669e4ba2
danwhite-lab <danielcwhite@gmail.com>
2026-09-22 09:16:58 +0300

    Add HAA-Simple Israel model

--- src/haa/strategies/__init__.py
+++ src/haa/strategies/__init__.py
@@ -2,5 +2,6 @@ from .haa_classic_no_qqq import HAAClassicNoQQQ
 from .haa_classic_leveraged_no_qqq import HAAClassicLeveragedNoQQQ
 from .haa_simple import HAASimple
 from .haa_simple_leveraged_2x import HAASimpleLeveraged2x
+from .haa_simple_israel import HAASimpleIsrael
 
-__all__ = ["HAAClassicNoQQQ", "HAAClassicLeveragedNoQQQ", "HAASimple", "HAASimpleLeveraged2x"]
+__all__ = ["HAAClassicNoQQQ", "HAAClassicLeveragedNoQQQ", "HAASimple", "HAASimpleLeveraged2x", "HAASimpleIsrael"]
