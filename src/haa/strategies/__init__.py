d0880c1ae20f6705c3cdb3e473551b7b9fede2ac
danwhite-lab <danielcwhite@gmail.com>
2026-09-25 19:17:35 +0300

    Register Inflation Compass Plus strategies

--- src/haa/strategies/__init__.py
+++ src/haa/strategies/__init__.py
@@ -1,10 +1,11 @@
 from .haa_4 import HAA4
 from .haa_4_leveraged_2x import HAA4Leveraged2x
 from .inflation_compass_steady import InflationCompassSteady
+from .inflation_compass_plus import InflationCompassPlus, InflationCompassPlusDriftBands
 from .haa_classic_no_qqq import HAAClassicNoQQQ
 from .haa_classic_leveraged_no_qqq import HAAClassicLeveragedNoQQQ
 from .haa_simple import HAASimple
 from .haa_simple_leveraged_2x import HAASimpleLeveraged2x
 from .haa_simple_israel import HAASimpleIsrael
 
-__all__ = ["HAA4", "HAA4Leveraged2x", "HAAClassicNoQQQ", "HAAClassicLeveragedNoQQQ", "HAASimple", "HAASimpleLeveraged2x", "HAASimpleIsrael", "InflationCompassSteady"]
+__all__ = ["HAA4", "HAA4Leveraged2x", "HAAClassicNoQQQ", "HAAClassicLeveragedNoQQQ", "HAASimple", "HAASimpleLeveraged2x", "HAASimpleIsrael", "InflationCompassSteady", "InflationCompassPlus", "InflationCompassPlusDriftBands"]
